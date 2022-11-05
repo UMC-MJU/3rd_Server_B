7b46d4184eea09d90959b044f7952d499c7c84be
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-06 04:01:51 +0900

    feat : UserAPI & MessageAPI

---
+++ 마이콜/CRUD/src/main/java/com/example/crud/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.crud.service;
+
+import com.example.crud.dto.RegisterDto;
+import com.example.crud.entity.User;
+import com.example.crud.repository.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User register(RegisterDto registerDto) {
+        User user = new User();
+        user.setNickname(registerDto.getNickname());
+        user.setPassword(registerDto.getPassword());
+        user.setUsername(registerDto.getUsername());
+        return userRepository.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findUser(int id) {
+        return userRepository.findById(id).orElseThrow(()-> {
+            return new IllegalArgumentException("찾을 ID를 다시 입력해주세요.");
+        });
+    }
+}
+
+
