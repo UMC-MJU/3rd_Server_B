8e61978447011f661361abb7210e0248bbb796f7
HaeChan Kim <bluesun147@naver.com>
2022-11-20 21:44:41 +0900

    [FEAT] paging

--- chan/Server_B--/src/main/java/com/umc/demo/post/PostController.java
+++ chan/Server_B--/src/main/java/com/umc/demo/post/PostController.java
@@ -1,12 +1,20 @@
 package com.umc.demo.post;
 
+import com.umc.demo.config.BaseException;
+import com.umc.demo.post.dto.PagingDto;
 import com.umc.demo.post.dto.PostDto;
 import com.umc.demo.post.dto.UpdateDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.umc.demo.config.BaseResponseStatus.DATABASE_ERROR;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,6 +22,8 @@ import java.util.Optional;
 public class PostController {
 
     private final PostService postService;
+    @Autowired
+    PostRepository postRepository;
 
     // 해당 유저 전체 포스트 조회
     @GetMapping("/all")
@@ -23,8 +33,12 @@ public class PostController {
 
     // id로 포스트 하나 조회
     @GetMapping("/{post_id}")
-    public Optional<Post> getPostById(@PathVariable int post_id) {
-        return postService.getPostById(post_id);
+    public Optional<Post> getPostById(@PathVariable int post_id) throws BaseException {
+        try {
+            return postService.getPostById(post_id);
+        } catch (BaseException exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 
     // 해당 유저의 모든 포스트 조회
@@ -51,5 +65,31 @@ public class PostController {
     @DeleteMapping("/{post_id}")
     public void updatePost(@PathVariable int post_id) {
         postService.deletePost(post_id);
+    }
+
+//    @GetMapping("/posts/search")
+//    public String search(String text, @PageableDefault(sort = "post_id", direction = Sort.Direction.DESC) Pageable pageable) {
+//        Page<Post> searchList = postService.search(text, pageable);
+//
+//        return String.valueOf(searchList);
+//    }
+
+    // https://semtax.tistory.com/77
+    // 페이징
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/post/page")
+    public Page<PagingDto> paging(@PageableDefault(size=5, sort="createdAt") Pageable pageRequest) {
+        // createdAt 기준으로 정렬
+        // 예시) http://localhost:8080/post/post/page?page=1&size=4 (size 안쓰면 디폴트는 5)
+        // 서비스로 빼는 것이 좋지만 연습용..
+        Page<Post> postList = postRepository.findAll(pageRequest);
+
+        Page<PagingDto> pagingList = postList.map(
+                post -> new PagingDto(
+                        post.getPost_id(),post.getText(),
+                        post.getCreatedAt()
+                ));
+
+        return pagingList;
     }
 }
--- chan/Server_B--/src/main/java/com/umc/demo/post/PostRepository.java
+++ chan/Server_B--/src/main/java/com/umc/demo/post/PostRepository.java
@@ -1,10 +1,16 @@
 package com.umc.demo.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByUserId(int user_id);
+
+    // <Post> findByText(String text, Pageable pageable);
+
+    // JPA 레포지토리에 Paging 함수 선언
+    Page<Post> findAll(Pageable pageable);
 }
